4c0ecae7582c94cd5c684a51f5b21b827247425a
Jonas Diemer <jonasdiemer@gmail.com>
2026-01-12 10:53:58 +0100

    Add option to hide battery percentage. (#297)
    with option to always hide or hide in reader only.
    
    Co-authored-by: Dave Allie <dave@daveallie.com>

--- src/activities/settings/SettingsActivity.cpp
+++ src/activities/settings/SettingsActivity.cpp
@@ -13,7 +13,7 @@
 
 // Define the static settings list
 namespace {
-constexpr int settingsCount = 18;
+constexpr int settingsCount = 19;
 const SettingInfo settingsList[settingsCount] = {
     // Should match with SLEEP_SCREEN_MODE
     {"Sleep Screen", SettingType::ENUM, &CrossPointSettings::sleepScreen, {"Dark", "Light", "Custom", "Cover", "None"}},
