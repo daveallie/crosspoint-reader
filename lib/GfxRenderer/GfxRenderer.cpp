950e2648adae94561648df3a9ba7695241c96cd4
Maeve Andrews <maeve@git.mail.maeveandrews.com>
2026-01-13 16:45:09 -0600

    Rotate button hints to portrait

--- lib/GfxRenderer/GfxRenderer.cpp
+++ lib/GfxRenderer/GfxRenderer.cpp
@@ -468,7 +468,10 @@ int GfxRenderer::getLineHeight(const int fontId) const {
 }
 
 void GfxRenderer::drawButtonHints(const int fontId, const char* btn1, const char* btn2, const char* btn3,
-                                  const char* btn4) const {
+                                  const char* btn4) {
+  const Orientation orig_orientation = getOrientation();
+  setOrientation(Orientation::Portrait);
+
   const int pageHeight = getScreenHeight();
   constexpr int buttonWidth = 106;
   constexpr int buttonHeight = 40;
@@ -487,6 +490,8 @@ void GfxRenderer::drawButtonHints(const int fontId, const char* btn1, const char
       drawText(fontId, textX, pageHeight - buttonY + textYOffset, labels[i]);
     }
   }
+
+  setOrientation(orig_orientation);
 }
 
 void GfxRenderer::drawSideButtonHints(const int fontId, const char* topBtn, const char* bottomBtn) const {
